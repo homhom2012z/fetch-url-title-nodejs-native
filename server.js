cb1ddfd622cae286099613116220676cb65b1956
homhom2012z <homhom2012z@gmail.com>
2022-03-15 01:31:45 +0700

    Add API usages & Fix typo: README.md

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const utilsRouter = require("./routes/utils");
 server.use("/api/v1", utilsRouter);
 
 server.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(
+    "<h2>Usages</h2><pre><code style='background-color: #eee; border: 1px solid #999; display: block; padding: 20px; width: 500px;'>GET: https://fetch-url-title.herokuapp.com/api/v1/title/?url={URL}</code></pre>"
+  );
 });
 
 const start = async () => {
