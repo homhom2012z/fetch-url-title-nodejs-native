e766c7d7ca49ebae53273dbd9b79126a9f27e473
homhom2012z <homhom2012z@gmail.com>
2022-03-31 20:53:06 +0700

    add(src): add link to source code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use("/api/v1", utilsRouter);
 
 server.get("/", (req, res) => {
   res.send(
-    "<h2>Usages</h2><pre><code style='background-color: #eee; border: 1px solid #999; display: block; padding: 20px; width: 500px;'>GET: https://fetch-url-title.herokuapp.com/api/v1/title/?url={URL}</code></pre>"
+    "<h2>Usages</h2><pre><code style='background-color: #eee; border: 1px solid #999; display: block; padding: 20px; width: 500px;'>GET: https://fetch-url-title.herokuapp.com/api/v1/title/?url={URL}</code></pre><a href='https://github.com/homhom2012z/fetch-url-title-nodejs-native'>source code</a>"
   );
 });
 
